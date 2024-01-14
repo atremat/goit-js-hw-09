46a65e1ff70d06b8d81691619c8d1d697b24f27d
Artem <atremat@gmail.com>
2024-01-14 03:15:03 +0200

    task 2 done

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -2,8 +2,6 @@ const form = document.querySelector('.feedback-form');
 const email = form.elements.email;
 const textarea = form.elements.message;
 const localStorageKey = 'feedback-form-state';
-const localEmail = 'email';
-const localText = 'message';
 
 //if localStorage isn't empty, then formData gets value from the localStorage
 //if localStorage is empty, then formData equals empty object
@@ -13,47 +11,26 @@ const formData = JSON.parse(localStorage.getItem(localStorageKey)) ?? {};
 email.value = formData.email ?? '';
 textarea.value = formData.message ?? '';
 
-// if (localStorage.getItem(localStorageKey)) {
-//   const parsedStorage = JSON.parse(localStorage.getItem(localStorageKey));
-//   email.value = parsedStorage.email;
-//   textarea.value = parsedStorage.textarea;
-// }
-// email.value = localStorage.getItem(localStorageKey) ?
-// localStorage.getItem(localStorageKey).email ?? '';
-// textarea.value = localStorage.getItem(localStorageKey).message ?? '';
-// email.value = localStorage.getItem(localEmail) ?? '';
-// textarea.value = localStorage.getItem(localText) ?? '';
-
 form.addEventListener('input', evt => {
-  // formData[evt.target.name] = evt.target.value;
-  // localStorage.setItem(localStorageKey, JSON.stringify(formData));
-  // localStorage.setItem(evt.target.name, evt.target.value);
-  // console.log(`evt.target.name = ${evt.target.name}`);
-  // console.log(`evt.target.value = ${evt.target.value}`);
-
-  //localStorage before:
-  // console.log('localStorage = ');
-  // console.log(localStorage);
-
   //adding input data to the object
   formData[evt.target.name] = evt.target.value;
-  // console.log('formData = ');
-  // console.log(formData);
 
   //converting object to JSON format
   const strFormData = JSON.stringify(formData);
-  // console.log('strFormData = ');
-  // console.log(strFormData);
 
   //adding formatted object to localstorage
-  localStorage.setItem(localStorageKey, strFormData); //отут перезаписуємо заново локалсторейдж
-  // console.log('localStorage = ');
-  // console.log(localStorage);
+  localStorage.setItem(localStorageKey, strFormData);
 });
 
 form.addEventListener('submit', evt => {
   evt.preventDefault();
-  console.log(evt.elements);
+
+  //output the object with fields "email", "message" and their values
+  console.log(formData);
+
+  //clear localstorage
   localStorage.removeItem(localStorageKey);
+
+  //clear fields of the form
   form.reset();
 });
